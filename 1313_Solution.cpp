54500589afe7a0844be423c2d8644b95415788ae
ZhouCihang <matthewzhou1128@gmail.com>
2022-02-07 17:58:44 +0800

    1313 vector easy insert()

--- 1313_Solution.cpp
+++ 1313_Solution.cpp
@@ -15,8 +15,14 @@ public:
     vector<int> decompressRLElist(vector<int>& nums) {
         vector<int> result;
 
-        for (int i = 0; i < nums.size(); i+=2){
-            result.insert(result.end(), nums[i], nums[i+1]);
+        // for (int i = 0; i < nums.size(); i+=2){
+        //     result.insert(result.end(), nums[i], nums[i+1]);
+        // }
+
+        for (int i=0; i<nums.size(); i += 2) {
+            for (int j=0; j<nums[i]; j++) {
+                result.push_back(nums[i+1]);
+            }
         }
 
         return result;
