e6535b25d3be0a1fd6c46512bc813dfbc2e7ffb7
ZhouCihang <matthewzhou1128@gmail.com>
2022-02-10 23:43:02 +0800

    387 hashmap() easy

---
+++ 387_Solution.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+#include <unordered_map>
+
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        unordered_map<char, int> map;
+        for(int i = 0; i < s.length(); i++){
+            if(map.count(s[i]) == 0){
+                map[s[i]] = 1;
+            }else{
+                map[s[i]]++;
+            }
+        }
+
+        for (int i = 0; i < s.length(); i++){
+            if(map[s[i]] == 1){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
+
+int main(){
+    Solution s;
+    cout << s.firstUniqChar("loveleetcode");
+    return 0;
+}
