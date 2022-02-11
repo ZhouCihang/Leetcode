c86b58bbebb6a4a79c67bf36895001d80fcc2c8a
ZhouCihang <matthewzhou1128@gmail.com>
2022-02-11 23:33:59 +0800

    1637 sort medium
    sort(vector<vector<int>>)

---
+++ 1637_Solution.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+
+void printVector(vector<vector<int> > &vect)
+{
+    for (int i = 0; i < vect.size(); i++)
+    {
+        for (int j = 0; j < vect[i].size(); j++)
+        {
+            cout << vect[i][j] << " ";
+        }   
+        cout << endl;
+    }
+}
+
+class Solution
+{
+public:
+    int maxWidthOfVerticalArea(vector<vector<int> > &points)
+    {
+        int ans = 0;
+        sort(points.begin(), points.end());
+        printVector(points);
+
+        for (int i = 1; i < points.size(); i++)
+        {
+            ans = max(ans, points[i][0] - points[i - 1][0]);
+        }
+        return ans;
+    }
+};
+
+void test()
+{
+    Solution solution;
+    vector<vector<int> > point{{3, 1}, {9, 0}, {1, 0}, {1, 4}, {5, 3}, {8, 8}};
+
+    cout << solution.maxWidthOfVerticalArea(point) << endl;
+}
+
+int main()
+{
+    test();
+    return 0;
+}
