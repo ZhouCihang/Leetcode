39cd206574c21e356b9b11583cc2f87d0ed53c74
ZhouCihang <matthewzhou1128@gmail.com>
2022-02-17 22:00:51 +0800

    1630 Binary search easy

--- 1630_Solution.cpp
+++ 1630_Solution.cpp
@@ -2,59 +2,68 @@
 using namespace std;
 #include <vector>
 
-void printVector(vector<bool> &v){
-    for (int i=0; i<v.size(); i++){
+void printVector(vector<bool> &v)
+{
+    for (int i = 0; i < v.size(); i++)
+    {
         cout << v[i] << " ";
     }
     cout << endl;
 }
 
-bool arithCheck(vector<int> &v){
-    int i = 0;
-    int temp = v[i+1] - v[i];
-
-    while(i < v.size() - 1){
-        if(v[i+1]-v[i] != temp){
-            return false;
-        }
-        i++;
-    }
-    return true;
-}
-
-class Solution {
+class Solution
+{
 public:
-    vector<bool> checkArithmeticSubarrays(vector<int>& nums, vector<int>& l, vector<int>& r) {
+    bool arithCheck(vector<int> &v)
+    {
+        int i = 0;
+        int temp = v[i + 1] - v[i];
+
+        while (i < v.size() - 1)
+        {
+            if (v[i + 1] - v[i] != temp)
+            {
+                return false;
+            }
+            i++;
+        }
+        return true;
+    }
+
+    vector<bool> checkArithmeticSubarrays(vector<int> &nums, vector<int> &l, vector<int> &r)
+    {
         int n = l.size();
         vector<bool> result;
         vector<int> temp;
 
-        for(int i = 0; i < n; i++) {
-            for(int j = l[i]; j <= r[i]; j++) {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = l[i]; j <= r[i]; j++)
+            {
                 temp.push_back(nums[j]);
             }
-            
+
             sort(temp.begin(), temp.end());
             result.push_back(arithCheck(temp));
-            printVector(result);
             temp.clear();
         }
         return result;
     }
 };
 
-void test(){
+void test()
+{
     Solution solution;
-    vector<int> nums{4,6,5,9,3,7};
-    vector<int> l{0,0,2};
-    vector<int> r{2,3,5};
+    vector<int> nums{4, 6, 5, 9, 3, 7};
+    vector<int> l{0, 0, 2};
+    vector<int> r{2, 3, 5};
     vector<bool> result;
 
-    result= solution.checkArithmeticSubarrays(nums,l,r);
-
+    result = solution.checkArithmeticSubarrays(nums, l, r);
 }
 
-int main(){
+int main()
+{
     test();
     return 0;
 }
