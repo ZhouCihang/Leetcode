3db81a244fadffe61a9c502cad0ddf8325ef391a
ZhouCihang <matthewzhou1128@gmail.com>
2022-02-17 14:37:11 +0800

    1630 sort medium

---
+++ 1630_Solution.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+
+void printVector(vector<bool> &v){
+    for (int i=0; i<v.size(); i++){
+        cout << v[i] << " ";
+    }
+    cout << endl;
+}
+
+bool arithCheck(vector<int> &v){
+    int i = 0;
+    int temp = v[i+1] - v[i];
+
+    while(i < v.size() - 1){
+        if(v[i+1]-v[i] != temp){
+            return false;
+        }
+        i++;
+    }
+    return true;
+}
+
+class Solution {
+public:
+    vector<bool> checkArithmeticSubarrays(vector<int>& nums, vector<int>& l, vector<int>& r) {
+        int n = l.size();
+        vector<bool> result;
+        vector<int> temp;
+
+        for(int i = 0; i < n; i++) {
+            for(int j = l[i]; j <= r[i]; j++) {
+                temp.push_back(nums[j]);
+            }
+            
+            sort(temp.begin(), temp.end());
+            result.push_back(arithCheck(temp));
+            printVector(result);
+            temp.clear();
+        }
+        return result;
+    }
+};
+
+void test(){
+    Solution solution;
+    vector<int> nums{4,6,5,9,3,7};
+    vector<int> l{0,0,2};
+    vector<int> r{2,3,5};
+    vector<bool> result;
+
+    result= solution.checkArithmeticSubarrays(nums,l,r);
+
+}
+
+int main(){
+    test();
+    return 0;
+}
