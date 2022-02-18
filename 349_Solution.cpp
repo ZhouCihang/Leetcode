970081bb1fab008b977037eb14242171ac14f26c
ZhouCihang <matthewzhou1128@gmail.com>
2022-02-18 16:25:06 +0800

    349 unordered_set find() & count() easy

---
+++ 349_Solution.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+#include <unordered_set>
+
+class Solution
+{
+public:
+
+    vector<int> getInterSec(unordered_set<int> &set1, unordered_set<int> &set2){
+        if(set1.size() > set2.size()){
+            return getInterSec(set2, set1);
+        }
+        vector<int> intSet;
+        for(auto& num:set1){
+            if(set2.count(num)){
+                intSet.push_back(num);
+            }
+        }
+        return intSet;
+    }
+
+    vector<int> intersection(vector<int> &nums1, vector<int> &nums2)
+    {
+        unordered_set<int> set1, set2;
+        vector<int> result;
+
+        for(int i = 0; i < nums1.size(); i++){
+            if(set1.find(nums1[i]) == set1.end()){
+                set1.insert(nums1[i]);
+            }
+        }
+        for(int i = 0; i < nums2.size(); i++){
+            if(set2.find(nums2[i]) == set2.end()){
+                set2.insert(nums2[i]);
+            }
+        }
+
+        return getInterSec(set1, set2);
+    }
+};
+
+void test()
+{
+    Solution solution;
+    vector<int> nums1{2,2}, nums2{1,2,2,1};
+    vector<int> resu;
+    resu = solution.intersection(nums1, nums2);
+}
+
+int main()
+{
+    test();
+    return 0;
+}
