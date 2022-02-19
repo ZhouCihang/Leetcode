5a345d25c3577162833f62a55149b1daae71ea27
ZhouCihang <matthewzhou1128@gmail.com>
2022-02-19 17:27:21 +0800

    save files

--- 349_Solution.cpp
+++ 349_Solution.cpp
@@ -6,14 +6,17 @@ using namespace std;
 class Solution
 {
 public:
-
-    vector<int> getInterSec(unordered_set<int> &set1, unordered_set<int> &set2){
-        if(set1.size() > set2.size()){
+    vector<int> getInterSec(unordered_set<int> &set1, unordered_set<int> &set2)
+    {
+        if (set1.size() > set2.size())
+        {
             return getInterSec(set2, set1);
         }
         vector<int> intSet;
-        for(auto& num:set1){
-            if(set2.count(num)){
+        for (auto &num : set1)
+        {
+            if (set2.find(num) != set1.end())
+            {
                 intSet.push_back(num);
             }
         }
@@ -25,13 +28,17 @@ public:
         unordered_set<int> set1, set2;
         vector<int> result;
 
-        for(int i = 0; i < nums1.size(); i++){
-            if(set1.find(nums1[i]) == set1.end()){
+        for (int i = 0; i < nums1.size(); i++)
+        {
+            if (set1.find(nums1[i]) == set1.end())
+            {
                 set1.insert(nums1[i]);
             }
         }
-        for(int i = 0; i < nums2.size(); i++){
-            if(set2.find(nums2[i]) == set2.end()){
+        for (int i = 0; i < nums2.size(); i++)
+        {
+            if (set2.find(nums2[i]) == set2.end())
+            {
                 set2.insert(nums2[i]);
             }
         }
@@ -43,7 +50,7 @@ public:
 void test()
 {
     Solution solution;
-    vector<int> nums1{2,2}, nums2{1,2,2,1};
+    vector<int> nums1{2, 2}, nums2{1, 2, 2, 1};
     vector<int> resu;
     resu = solution.intersection(nums1, nums2);
 }
