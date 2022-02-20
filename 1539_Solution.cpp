bf0fb504e6acedf7b12f567880e61cbf7e16a998
ZhouCihang <matthewzhou1128@gmail.com>
2022-02-20 21:02:31 +0800

    1539 Binary Search Easy
    https://leetcode-cn.com/problems/kth-missing-positive-number/solution/di-k-ge-que-shi-de-zheng-zheng-shu-by-leetcode-sol/

---
+++ 1539_Solution.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+
+class Solution
+{
+public:
+    int findKthPositiveMethod2(vector<int> &arr, int k)
+    {
+        vector<int> res(1000, 0);
+        for (auto &i : arr)
+        {
+            res[i] = 1;
+        }
+
+        int count = 0, result = -1;
+        for (int i = 1; i <= res.size(); i++)
+        {
+            if (res[i] == 0)
+            {
+                count++;
+            }
+            if (count == k)
+            {
+                result = i;
+                break;
+            }
+        }
+        return result;
+    }
+    int findKthPositive(vector<int> &arr, int k)
+    {
+        if (k < arr[0])
+            return k;
+        
+        int low(0), high(arr.size());
+        cout <<low << " " <<high<<endl;
+        while (low < high)
+        {
+            int mid = low + ((high - low) >> 1);
+            if (arr[mid] - mid - 1 < k)
+                low = mid + 1;
+            else
+                high = mid;
+        }
+        return low + k;
+    }
+};
+
+void test()
+{
+    Solution solution;
+    vector<int> arr{1, 2, 3, 4};
+    cout << solution.findKthPositive(arr, 2) << endl;
+}
+
+int main()
+{
+    test();
+    return 0;
+}
