fbb0233fdd8246c124493d85d5edc8f82af9d286
ZhouCihang <matthewzhou1128@gmail.com>
2022-02-20 19:40:04 +0800

    888 unordered_map easy

---
+++ 888_Solution.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+#include <unordered_set>
+#include <numeric>
+
+class Solution {
+public:
+    vector<int> fairCandySwap(vector<int>& aliceSizes, vector<int>& bobSizes) {
+
+        int sumAlice = accumulate(aliceSizes.begin(), aliceSizes.end(), 0);
+        int sumBob = accumulate(bobSizes.begin(), bobSizes.end(), 0);
+
+        int delta = (sumAlice - sumBob) / 2;
+
+        // unordered_map<int, int> mapAlice;
+        // unordered_map<int, int>::iterator it;
+        // for (int i = 0; i < aliceSizes.size(); i++){
+        //     mapAlice[i] = aliceSizes[i];
+        // }
+        unordered_set<int> mapAlice(aliceSizes.begin(), aliceSizes.end());
+        vector<int> result;
+        for(int i = 0; i < bobSizes.size(); i++){
+            int val = bobSizes[i]+delta;
+            if(mapAlice.find(val) != mapAlice.end()){
+                result = vector<int>{val, bobSizes[i]};
+                break;
+            }
+        }
+
+        return result;
+    }
+};
+
+void test(){
+    Solution solution;
+    vector<int> aliceSizes{2}, bobSizes{1,3};
+    vector<int> result;
+    result = solution.fairCandySwap(aliceSizes, bobSizes);
+
+    for (int i = 0; i < result.size(); i++){
+        cout<< result[i] << " ";
+    }
+    cout << endl;
+}
+
+int main(){
+    test();
+    return 0;
+}
