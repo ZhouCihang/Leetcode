0a7a41e38752df334afa02c0f4c5167116200106
ZhouCihang <matthewzhou1128@gmail.com>
2022-02-24 16:21:42 +0800

    2000 reverse string easy

---
+++ 2000_Solution.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+
+class Solution {
+public:
+    string reversePrefix(string word, char ch) {
+        for(int i=0; i<word.length(); i++){
+            if(word[i]==ch){
+                reverse(word.begin(), word.begin()+i+1);
+                return word;
+            }
+        }
+        return word;
+    }
+};
+
+int main(){
+    Solution solution;
+    cout << solution.reversePrefix("xyxzxe", 'z') << endl;
+    return 0;
+}
