e31adff20a179bdf192138a1213a61464a8946d4
ZhouCihang <matthewzhou1128@gmail.com>
2022-03-03 17:37:04 +0800

    70 recursive or simple fibonacci method easy

---
+++ 70_Solution.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+class Solution
+{
+public:
+    int res = 0;
+    int climbStairs(int n)
+    {
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
+        if (n == 2)
+            return 2;
+        return climbStairs(n - 1) + climbStairs(n - 2);
+    }
+
+    int climbStairs2(int n)
+    {
+        if (n <= 2)
+            return n;
+        int prev = 2, prev2 = 1, res;
+        for (int i = 3; i <= n; i++)
+        {
+            res = prev + prev2;
+            prev2 = prev;
+            prev = res;
+        }
+        return res;
+    }
+};
+
+int main()
+{
+    Solution solution;
+    cout << solution.climbStairs2(15) << endl;
+    return 0;
+}
