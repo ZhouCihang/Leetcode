e74d50b92af89e27cf7e9569e0f53e4f6fb24960
ZhouCihang <matthewzhou1128@gmail.com>
2022-03-14 19:00:53 +0800

    34 重写这题， 注重上边界和下边界的条件

--- 34_Solution.cpp
+++ 34_Solution.cpp
@@ -40,12 +40,47 @@ public:
         }
         return result;
     }
+
+    int lowerBound(vector<int>& nums, int target){
+        int left = 0, right = nums.size() - 1, mid;
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+            if(nums[mid] < target) left = mid + 1;
+            else if(nums[mid] >= target) right = mid - 1;
+        }
+        return left;
+    }
+
+    int upperBound(vector<int>& nums, int target){
+        int left = 0, right = nums.size() - 1, mid;
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+            if(nums[mid] <= target) left = mid + 1;
+            else if(nums[mid] > target) right = mid - 1;
+        }
+        return right;
+    }
+    vector<int> searchRange2(vector<int>& nums, int target) {
+        int upper = upperBound(nums, target);
+        int lower = lowerBound(nums, target);
+        vector<int> result(2, -1);
+        
+        if(upper < lower) return result;
+        result[0] = lower, result[1] = upper;
+        return result;
+    }
 };
+/*计算上边界时算是和计算上边界时条件相反，
+
+计算下边界时，当 target <= nums[mid] 时，right = mid -1；target > nums[mid] 时，left = mid + 1；
+
+计算上边界时，当 target < nums[mid] 时，right = mid -1; target >= nums[mid] 时 left = mid + 1;刚好和计算下边界时条件相反，返回right。
+*/
 
 int main(){
     Solution s;
     vector<int> nums{5,7,7,8,8,10};
     vector<int> result;
-    result = s.searchRange(nums, 8);
+    result = s.searchRange2(nums, 8);
     return 0;
 } 
