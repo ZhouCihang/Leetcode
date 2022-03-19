934c926bae35d740215afacc17b1497dea9bc41e
ZhouCihang <matthewzhou1128@gmail.com>
2022-03-19 16:29:33 +0800

    1254 Number of Closed Islands

---
+++ 1254_Solution.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+
+class Solution
+{
+public:
+    int closedIsland(vector<vector<int> > &grid)
+    {
+        int result = 0;
+        int m = grid.size(), n = grid[0].size();
+
+        for (int j = 0; j < n; j++)
+        {
+            dfs(grid, 0, j);
+            dfs(grid, m - 1, j);
+        }
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                cout << grid[i][j] << " ";
+            }
+            cout << endl;
+        }
+
+        for (int i = 0; i < m; i++)
+        {
+            dfs(grid, i, 0);
+            dfs(grid, i, n - 1);
+        }
+
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (grid[i][j] == 0)
+                {
+                    result += 1;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+        return result;
+    }
+    void dfs(vector<vector<int> > &grid, int i, int j)
+    {
+        if (i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size())
+        {
+            return;
+        }
+        if (grid[i][j] == 1)
+            return;
+        grid[i][j] = 1;
+        dfs(grid, i - 1, j);
+        dfs(grid, i + 1, j);
+        dfs(grid, i, j - 1);
+        dfs(grid, i, j + 1);
+    }
+};
+
+int main()
+{
+    Solution s;
+    vector<vector<int> > grid{{1, 0, 0, 0, 0, 0, 0, 0},
+                               {1, 0, 0, 0, 0, 1, 1, 0},
+                               {1, 0, 1, 0, 1, 1, 1, 0},
+                               {1, 0, 0, 0, 0, 1, 0, 1},
+                               {1, 1, 1, 1, 1, 1, 1, 0}};
+    cout << s.closedIsland(grid);
+    return 0;
+}
