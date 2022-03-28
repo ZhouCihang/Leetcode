32371fa9ff2d78e07827ca70d528854faf623d12
ZhouCihang <matthewzhou1128@gmail.com>
2022-03-28 17:32:33 +0800

    53 Max SubArray with DP method

--- 53_Solution.cpp
+++ 53_Solution.cpp
@@ -10,18 +10,39 @@ public:
         int sum = 0;
         int maxResult = 0;
         int left = 0;
-        for (int i = 0; i < nums.size(); ++i){
+        for (int i = 0; i < nums.size(); ++i)
+        {
             sum += nums[i];
-            while (sum < 0){
+            while (sum < 0)
+            {
                 sum -= nums[left++];
             }
             maxResult = max(sum, maxResult);
         }
         int maxVal = nums[0];
-        for(int i = 1; i< nums.size(); ++i){
+        for (int i = 1; i < nums.size(); ++i)
+        {
             maxVal = max(maxVal, nums[i]);
         }
         return maxVal < 0 ? maxVal : maxResult;
+    }
+    int maxSubArrayDP(vector<int> &nums)
+    {
+        int prev = 0, maxAns = nums[0];
+        for (int i = 1; i < nums.size(); ++i)
+        {
+            prev = max(prev + nums[i], nums[i]);
+            maxAns = max(maxAns, prev);
+        }
+        return maxAns;
+    }
+    int maxSubArrayDP2(vector<int> &nums)
+    {
+        for (int i = 1; i < nums.size(); ++i)
+        {
+            if(nums[i-1] > 0) nums[i] += nums[i-1];
+        }
+        return *max_element(nums.begin(), nums.end());
     }
 };
 
@@ -29,6 +50,6 @@ int main()
 {
     Solution solution;
     vector<int> nums{-2, 1, -3, 4, -1, 2, 1, -5, 4};
-    cout << solution.maxSubArray(nums) << endl;
+    cout << solution.maxSubArrayDP2(nums) << endl;
     return 0;
 }
