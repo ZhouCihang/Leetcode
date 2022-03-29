2b4d8947e3f85784d19ae727889da5916dc58226
ZhouCihang <matthewzhou1128@gmail.com>
2022-03-29 15:41:02 +0800

    560 Subarray Sum Equals K
    Prefix + hash map

--- 560_Solution.cpp
+++ 560_Solution.cpp
@@ -37,16 +37,40 @@ public:
         preSum[0] = 1;
 
         int ans = 0, sum0_i = 0;
-        for(int i = 0; i<n; i++){
+        for (int i = 0; i < n; i++)
+        {
             sum0_i += nums[i];
-            
+
             int sum0_j = sum0_i - k;
-            if(preSum.count(sum0_j)){
+            if (preSum.count(sum0_j))
+            {
                 ans += preSum[sum0_j];
             }
             preSum[sum0_i]++;
         }
         return ans;
+    }
+
+    int subarraySumMethod3(vector<int> &nums, int k)
+    {
+        int n = nums.size();
+        int prefix[n];
+        prefix[0] = nums[0];
+
+        for (int i = 1; i < n; ++i)
+        {
+            prefix[i] = prefix[i - 1] + nums[i];
+        }
+        unordered_map<int, int> mp;
+        int count = 0;
+        for(int i = 0; i < n; ++i){
+            if(prefix[i] == k) count++;
+            if(mp.find(prefix[i] - k) != mp.end()){
+                count += mp[prefix[i] - k];
+            }
+            mp[prefix[i]]++;
+        }
+        return count;
     }
 };
 
@@ -54,7 +78,7 @@ void test()
 {
     Solution solution;
     vector<int> nums{1, 2, 3};
-    cout << solution.subarraySumMethod2(nums, 2) << endl;
+    cout << solution.subarraySumMethod3(nums, 2) << endl;
 }
 
 int main()
