17d30e5e7fe4d7b3d09564a5b6cedfec83019b11
ZhouCihang <matthewzhou1128@gmail.com>
2022-04-01 15:57:06 +0800

    75 快排分区算法+左右双指针

--- 75_Solution.cpp
+++ 75_Solution.cpp
@@ -2,26 +2,55 @@
 using namespace std;
 #include <vector>
 
-class Solution {
+class Solution
+{
 public:
-    void sortColors(vector<int>& nums) {
-        for(int i=0; i<nums.size()-1; ++i){
-            for(int j = i+1; j < nums.size(); ++j){
-                if(nums[i] > nums[j])
+    void sortColors(vector<int> &nums)
+    {
+        for (int i = 0; i < nums.size() - 1; ++i)
+        {
+            for (int j = i + 1; j < nums.size(); ++j)
+            {
+                if (nums[i] > nums[j])
                     swap(nums[i], nums[j]);
             }
         }
 
-        for(int i=0; i<nums.size(); ++i){
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            cout << nums[i] << " ";
+        }
+        cout << endl;
+    }
+    void sortColorsPartition(vector<int> &nums)
+    {
+        if(nums.size() < 2) return;
+        int p0 = 0, i = 0, p2 = nums.size() - 1;
+        while(i <= p2){
+            if(nums[i] == 0){
+                swap(nums[i], nums[p0]);
+                i++;
+                p0++;
+            }else if(nums[i] == 1){
+                i++;
+            }else{
+                swap(nums[i], nums[p2]);
+                p2--;
+            }
+        }
+
+        for (int i = 0; i < nums.size(); ++i)
+        {
             cout << nums[i] << " ";
         }
         cout << endl;
     }
 };
 
-int main(){
+int main()
+{
     Solution s;
-    vector<int> nums{2,0,2,1,1,0};
+    vector<int> nums{2, 0, 2, 1, 1, 0};
     s.sortColors(nums);
     return 0;
 }
