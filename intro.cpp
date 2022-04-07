7eaf0c16eb290d488bc8af8b93e622b099cf2395
ZhouCihang <matthewzhou1128@gmail.com>
2022-04-07 16:19:13 +0800

    Sizeof on basic datatype

--- intro.cpp
+++ intro.cpp
@@ -3,6 +3,8 @@ using namespace std;
 
 int main(){
     cout<<"Creating this new Repo for Leetcode Practice!"<<endl;
+    long long a;
+    cout<< sizeof(a)<<endl;
     cout<<endl;
     return 0;
 }
