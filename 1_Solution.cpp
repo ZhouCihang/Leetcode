73bd8be323320e715b39bb1ffc50fe5ccaeab7d3
ZhouCihang <matthewzhou1128@gmail.com>
2022-04-18 18:05:40 +0800

    rewrite for amazon VO

--- 1_Solution.cpp
+++ 1_Solution.cpp
@@ -8,6 +8,7 @@ class Solution
 public:
     vector<int> twoSum(vector<int> &nums, int target)
     {
+        //key is nums[i], value is i;
         unordered_map<int, int> stored;
         for(int i = 0; i < nums.size(); ++i){
             auto it = stored.find(target - nums[i]);
@@ -24,5 +25,9 @@ int main()
     vector<int> nums{2,7,11,15};
     vector<int> result;
     result = solution.twoSum(nums, 9);
+    for(int i = 0; i < result.size(); ++i){
+        cout << result[i]<<" ";
+    }
+    cout << endl;
     return 0;
 }
