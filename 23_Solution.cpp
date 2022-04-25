99dc1612e16e38c088c6c3d5be844c89b78dba5c
ZhouCihang <matthewzhou1128@gmail.com>
2022-04-25 16:33:27 +0800

    redo 23 Merge K Sorted Lists (Priority queue)

--- 23_Solution.cpp
+++ 23_Solution.cpp
@@ -53,8 +53,38 @@ public:
         }
 
         tail->next = NULL;
-        return dummyHead.next;
+        return dummyHead->next;
     }
+
+    ListNode *mergeKListsRedo(vector<ListNode *> &lists){
+        if(lists.empty() || lists.size() == 0) return nullptr;
+        
+        auto cmp = [&](ListNode *list1, ListNode *list2){
+            return list1->val > list2->val;
+        };
+        priority_queue<ListNode *, vector<ListNode *>, decltype(cmp)> q(cmp);
+        for(int i = 0; i < lists.size(); ++i){
+            if(lists[i]){
+                q.push(lists[i]);
+            }
+        }
+
+        ListNode *dummyHead = new ListNode(0);
+        ListNode *tail = dummyHead;
+        while(!q.empty()){
+            auto temp = q.top();
+            q.pop();
+            tail->next = temp;
+            tail = tail->next;
+            if(tail->next){
+                q.push(temp->next);
+            }
+        }
+        tail->next = nullptr;
+        return dummyHead->next;
+
+    }
+
 };
 
 int main()
